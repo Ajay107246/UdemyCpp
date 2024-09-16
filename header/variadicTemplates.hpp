299dcfe4f71c35b16bd23810c9a5750bfda22417
Ajay Shimpi <shimpi.ajay.24@gmail.com>
2024-09-16 13:28:06 +0200

    commit#38: Topic66-2, Variadic Template, any num of type and args, reduced args. (0 arg with function gives error)

--- header/variadicTemplates.hpp
+++ header/variadicTemplates.hpp
@@ -12,10 +12,85 @@ using namespace std;
     can not accepts references as an arguments
     1.  this can be implement using c++11 -> initializer_list
         with initializer_list args have to be of same type
+    2. using variadic template, can write a function accept any type ad number of argument
+    e.g. 
+    //template param pack
+    template<typename... Params>
+    ... -> ellipsis and used in varies places in cpp
+    it senifies a variadic template, need to mentioned name that will represent variable num of type name
+    Params doesn't represent typename, rather it is an alias to the list of typenames
+    (pack of template typename)
+    further Params will used in function to signify the arguments of function
+
+    -> used in call catch block in exception handling
+
 */
 
+//function to stop recursion once num of args = 0 
+void PrintVariadic()
+{   
+    /*  leads to error: 
+        multiple definition of `PrintVariadic()'; C:\Users\Shimpi\AppData\Local\Temp\cc5oVYX1.o:G:/CPP/2024/VSCode/UdemyCpp/header/
+        variadicTemplates.hpp:31: first defined here
+        collect2.exe: error: ld returned 1 exit status
+        Build finished with error(s)
+    */
+}
+
+//Template param pack
+template<typename T, typename... Params>
+//Function parma pack
+void PrintVariadic(T a, Params... args)
+{
+    /* 
+        1. using variadic template with any number of type and args
+        template<typename... Params>
+        (1, 2.5, 3, '4') -> args will expanded automatically by compiler at -> (Params... args)
+        void PrintVariadic(Params... args)
+        
+        2. we can not access individual arg directly, instead rely on recursion
+        in each recursive call the num of args will reduced by one.
+        also, just like normal recursion to end the operation, we need to condition.
+        
+        3. here, in variadic template function, that will done by writing another PrintVariadic() function
+        without using variadic template -> called as base case function
+        ** write PrintVariadic() function to pass function parameter pack (args...)
+        PrintVariadic(args...);
+        
+        4. compiler will invoke PrintVariadic() with same args that were passed in PrintVariadic()
+        ** but compiler give a warning : runtime stackover flow because of infinite recursion
+        so we need write a base case that will stop recursion.
+        by passing one more arg to PrintVariadic() function.
+        template<typename T, typename... Params>
+        void PrintVariadic(T a, Params... args)
+
+        5. This will give an error, 
+            no matching fucntion call to 'PrintVariadic()' [expected 2 args and provided 0 arg]
+        in each recursive call, the num of args is reduced by one, and PrintVariadic() will invoke 0 num of arg
+        -> PrintVariadic(1, 2.5, 3, '4');
+            1 : T a,  2.5, 3, '4' : (Params... args) fun param pack.
+        
+        -> PrintVariadic(2.5, 3, '4');
+            num of param reduced by one
+            2.5 : T a,  && 3, '4' : (Params... args) fun param pack.
+
+        -> PrintVariadic(3, '4');
+        
+        -> PrintVariadic('4');
+            '4' : T a -> here, when PrintVariadic(args...) invoked with 0 num of args
+
+        -> PrintVariadic(); 
+            using template instantiation, compiler will look for a function PrintVariadic() does not accept any arg
+            and we dnt have a such function, but we can write PrintVariadic() with no args,
+            this function will be act as base case function to stop recursion
+    */
+   PrintVariadic(args...);
+
+}
+
+
 template<typename T>
-void PrintVariadic(initializer_list<T> args)
+void PrintInitList(initializer_list<T> args)
 {
     /* using range based for loop to access the int elements */
     for (const auto &x : args)
--- source/variadicTemplates.cpp
+++ source/variadicTemplates.cpp
@@ -3,6 +3,8 @@
 
 void all_variadicTemplates_method()
 {
-    PrintVariadic({1,2,3,4,5});
+    PrintInitList({1,2,3,4,5}); //using initializer_lists
+    
+    PrintVariadic(1, 2.5, 3, '4');    //using variadic template with any number of type and args
     cout << endl;
 }
