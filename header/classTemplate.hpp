b1e5d8b7f0b061c18a131335fa8259ddabf7a6b7
Ajay Shimpi <shimpi.ajay.24@gmail.com>
2024-09-18 15:17:21 +0200

    commit#43: Topic67-2, class template, method definition outside class, default & copy constr

--- header/classTemplate.hpp
+++ header/classTemplate.hpp
@@ -2,38 +2,84 @@
 #ifndef CLASSTEMPLATE__H
 #define CLASSTEMPLATE__H
 
-using namespace std;
+#include <iostream>
+#include <array>
+
+//using namespace std;
 
 /*
     classes that should be able to handle the different types, i.f. class template is used with containers
     e.g. basic Stack class example to understand the need of variable data type by using class template 
+
+    1. same Stack class can be used for different class type
+    2. compiler will instantiate stk class for all the types that it is instantiated for
+    3. only those functions are instantitated that are invoked. if dn't invoke some of member functions, then they are not instantiated
+    4. Class template can also accept non-type template args -> int size is specified here.
+    5. e.g. Stack<float, 10> stFlSize;
+    6. this is much better than using const value in member variable
+    7. while defining the method outside of the class, we need to mentioned with template and class with size
+    8. 
+    e.g.{ void Pop(); }
+    template<typename T, int size>
+    void Stack<T, size>::Pop(){--m_Top;}
+    9. factory method (method to create instance of class) for this Stack class
+    
+    link: https://sl.bing.net/i3pXXDkhvP2 
 */
 
+
+template<typename T, /*int*/std::size_t size>  // can take size as arg to obj
 class Stack
 {
     private:
-    int m_Buffer[512];
-    int m_Top{-1};
+    //T m_Buffer[size]; //error: array subscript is not an integer
+    std::array<T, size> m_Buffer;
+    T m_Top {-1};   // int type can be used instead T
     
     public:
-    Stack();
-    void Push(int elem);
-    void Pop();
-    int Top()const;
-    bool isEmpty();
-    ~Stack();
-};
+    /*
+        Explicitly defaulted constructor, where compiler will generate its own constr
+        To ensure that the compiler generates the default constructor even if other constructors are defined
+        Make code more readable and explicit about the presence of a default constructor
+        To control the accessibility of the default constructor (e.g., making it public, protected, or private)
+    */
+    Stack() = default;  
+    /*
+        copy constr
+        //arg -> uses shorthand notation, since appears within class
+        long hand notation can be used.
+        Stack(const Stack<T, size> &obj);    
+    */
+    Stack(const Stack &obj);    
 
+    void Push(const T &elem);   //can be user defined type , passed by const reference
+    void Pop();
+    const T& Top()const;
+    bool isEmpty() const;
+
+    /* 
+        factory method, that creates and return instance of Stack class
+        this is short hand notation since inside class, do not need to mentioned template paramter
+        can be written only if that type occurs inside class definition
+        can not write shorthand definition outside the class, have to use complete type class in that definition outside
+        error: deduced class type 'Stack' in function return type
+        i.e. shorthand notation ony works in side class
+
+        need all template parameter at return type in definition
+    */
+    static Stack Create();
+
+    ~Stack() noexcept;  //noexcept: good practice in c++
+};
 class classTemplate
 {
 private:
-    Stack st;
+    //Stack st; //67-1 without template class example
+    Stack<float, 10> stFloat = Stack<float, 10>::Create(); //67-2
 public:
-    classTemplate(/* args */);
-
+    classTemplate();
     void all_classTemplate_methods();
-    
-    ~classTemplate();
+    ~classTemplate() noexcept;  //noexcept: good practice in c++
 };
 
 
--- source/classTemplate.cpp
+++ source/classTemplate.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include "../header/classTemplate.hpp"
 
-using namespace std;
+//using namespace std; //to avoid to prevent namespace pollution
 
 void classTemplate::all_classTemplate_methods()
 {
+    /*//67-1 without template class example
     st.Push(3);
     st.Push(2);
-    st.Push(4);
+    st.Push(4); 
     st.Push(7);
 
     while (!st.isEmpty())
     {
-        cout << st.Top() << ' ';
+        std::cout << st.Top() << ' ';
         st.Pop();
     }
     /*  output1:
@@ -25,18 +26,46 @@ void classTemplate::all_classTemplate_methods()
         distructor, classTemplate::~classTemplate()
         distructor, Stack::~Stack()
     */
+    stFloat.Push(3);
+    stFloat.Push(2);
+    stFloat.Push(4); 
+    stFloat.Push(7);
+    auto stFloatCpy(stFloat);
+
+    while (!stFloatCpy.isEmpty())
+    {
+        std::cout << stFloatCpy.Top() << ' ';
+        stFloatCpy.Pop();
+    }
+    /*
+        67-2, output:     
+        Topic67: Class Templates in c++
+        constr, Stack<T, size>::Stack()
+        constr, classTemplate::classTemplate()
+        7 4 2 3
+
+        !End of Main()!
+        distructor, classTemplate::~classTemplate()
+        distructor, Stack<T, size>::~Stack()
+
+        with explicite constr (compiler generated)
+        Class Templates in c++
+        constr, classTemplate::classTemplate()
+        7 4 2 3 
+        distructor, Stack<T, size>::~Stack()
+    */
 }
 
 classTemplate::classTemplate()
 {
-    cout << "constr, classTemplate::classTemplate()" << endl;
+    std::cout << "constr, classTemplate::classTemplate()" << std::endl;
 }
 
 classTemplate::~classTemplate()
 {
-    cout << "distructor, classTemplate::~classTemplate()" << endl;
+    std::cout << "distructor, classTemplate::~classTemplate()" << std::endl;
 }
-
+/*
 Stack::Stack()
 {
     cout << "constr, Stack::Stack()" << endl;
@@ -65,4 +94,61 @@ bool Stack::isEmpty()
 Stack::~Stack()
 {
     cout << "distructor, Stack::~Stack()" << endl;
+}
+*/
+
+/*
+//note: required if Stack(); constr is used
+template <typename T, std::size_t size>
+Stack<T, size>::Stack()
+{
+    std::cout << "constr, Stack<T, size>::Stack()" << std::endl;
+}*/
+
+template <typename T, std::size_t size>
+Stack<T, size>::Stack(const Stack &obj)
+{
+    m_Top = obj.m_Top;
+    for(int i = 0; i<=m_Top; ++i) //since top start from -1, use <=
+    {
+        m_Buffer[i] = obj.m_Buffer[i];
+    }
+}
+
+template <typename T, std::size_t size>
+void Stack<T, size>::Push(const T &elem)
+{
+    m_Buffer[++m_Top] = elem;   //error: array subscript is not an integer
+}
+
+template <typename T, std::size_t size>
+void Stack<T, size>::Pop()
+{
+    --m_Top;
+}
+
+template <typename T, std::size_t size>
+const T &Stack<T, size>::Top() const
+{
+    return m_Buffer[m_Top]; //error: array subscript is not an integer, fixed with inlude array, and init in class
+}
+
+template <typename T, std::size_t size>
+bool Stack<T, size>::isEmpty() const
+{
+    return m_Top == -1;
+}
+
+
+/* factory method, that creates and return instance of Stack class */
+template <typename T, std::size_t size>
+Stack<T, size> Stack<T, size>::Create() //compiler doen't know type of return value -> Stack , provided param
+{
+    return Stack<T, size>();    //create temp obj of Stack, returning by value, use it further 
+}
+
+template <typename T, std::size_t size>
+Stack<T, size>::~Stack() noexcept
+{
+    std::cout << "\ndistructor, Stack<T, size>::~Stack()" << std::endl;
 }
